501904fad33efb0af965d183a702193f1c873c77
Penguin.jpg <sky026912@gmail.com>
2023-04-09 21:35:54 +0800

    gronsfeld修正錯誤註解且現在可以處理大寫字母

--- gronsfeld.py
+++ gronsfeld.py
@@ -10,13 +10,13 @@ import random
 """
 目前構想步驟: 
  - 階段1(準備初始key):
-    1. 先將key中的英文全部轉數字(0~25)
+    1. 先將key中的每個英文轉成對應的index
     2. 將字串打亂
     3. 隨機分割key成k個長度1或2的group
     4. 將每個group的值按照以下規則替換成英文
-        - 長度1: 將該字元轉數字mod26
-        - 長度2: 將字元a,b轉數字後相乘並加上randint(a,b)後mod26
-    5. 由於合併完後的key長度會變小，所以將key用a-z的隨機值補到原長度
+        - 長度1: 將該字元轉數字mod52
+        - 長度2: 將字元a,b轉數字後相乘並加上randint(a,b)後mod52
+    5. 由於合併完後的key長度會變小，所以將key用[a-zA-Z]的隨機值補到原長度
  - 階段2(Autokey):
     1. 從明文中取字元，共取len(plaintext)-len(key)個
     2. 將英文全部轉數字
@@ -26,15 +26,16 @@ import random
 
 
 class Gronsfeld:
-    def __init__(self, key, alphabet=string.ascii_lowercase):
-        self.alphabet = alphabet
+    def __init__(self, key):
+        self.AVAILABLE_CHARS = string.ascii_letters
+        self.char_set = set(self.AVAILABLE_CHARS)
         self.key = key
         self._prepare_key(len(key))
 
     def _key_to_all_nums(self):
         """Turn every char in key to number"""
-        for i, letter in enumerate(self.alphabet):
-            self.key = self.key.replace(letter, str(i))
+        for i, char in enumerate(self.AVAILABLE_CHARS):
+            self.key = self.key.replace(char, str(i))
         print("numeric key: " + self.key)
 
     def _shuffle_key(self):
@@ -42,30 +43,31 @@ class Gronsfeld:
         print("shuffle key: " + self.key)
 
     def _random_split(self):
-        """Randomly split key into k groups within range [0,25] and turn them to letters"""
+        """Randomly split key into k groups within range [1,2] and turn them to letters"""
         splited_key = []
         while len(self.key) > 0:
             split = random.randint(1, 2)
             if split == 2 and len(self.key) >= 2:
-                # if split to length 2, use (a * b + randint(a, b)) % 26 as new key
+                # if split to length 2, use (a * b + randint(a, b)) % 52 as new key
                 a, b = map(int, self.key[:split])
                 if a > b:  # make sure a < b
                     a, b = b, a
                 offset = random.randint(a, b)
-                splited_key.append((a * b + offset) % 26)
+                splited_key.append((a * b + offset) % len(self.AVAILABLE_CHARS))
             else:
-                # if split to length 1, use this char % 26 as new key
-                splited_key.append(int(self.key[:split][0]) % 26)
+                # if split to length 1, use this char % 52 as new key
+                splited_key.append(int(self.key[:split][0]) % len(self.AVAILABLE_CHARS))
             self.key = self.key[split:]
         print(f"splited key: {splited_key}")
-        self.key = "".join(self.alphabet[key] for key in splited_key)
+        self.key = "".join(self.AVAILABLE_CHARS[key] for key in splited_key)
         print("random split key: " + self.key)
 
     def _pad_to_target_length(self, length):
         """Pad random alphabet to reach specific length"""
         length_diff = length - len(self.key)
         for _ in range(length_diff):
-            self.key += self.alphabet[random.randint(0, 25)]
+            # randomly select a char from AVAILABLE_CHARS
+            self.key += random.choice(self.AVAILABLE_CHARS)
         print("padded key: " + self.key)
 
     def _prepare_key(self, target_length):
@@ -83,69 +85,55 @@ class Gronsfeld:
         self._shuffle_key()
         print("autokey: " + self.key)
 
-    def encrypt(self, plaintext):
-        """Encrypt plaintext using Vigenere cipher
-
-        :param plaintext: Plaintext to encrypt
-        :return: Encrypted plaintext
-        """
-        self._autokey(plaintext)
-        return self.rotate_string(plaintext)
-
-    def decrypt(self, ciphertext):
-        """Decrypt plaintext using Vigenere cipher
-
-        :param ciphertext: Ciphertext to decrypt
-        :return: Decrypted ciphertext
-        """
-        return self.rotate_string(ciphertext, True)
-
-    def rotate_string(self, string, reverse=False):
-        """Shift a string using a key
-
-        :param string: String to shift using the key
-        :param reverse:
-        :return:
-        """
+    def _rotate_string(self, string, reverse=False):
+        """Shift a string using a key (use reverse=True for decrypt)"""
         key_pos = 0
         shifted = ""
 
         # Iterate through each char in the plaintext
         for char in string:
-            # Ignore characters not in the alphabet
-            if char not in self.alphabet:
+            # Ignore characters not in the AVAILABLE_CHARS
+            if char not in self.char_set:
                 shifted += char
                 continue
 
-            # Get the position in the alphabet of that char
-            pos = self.alphabet.index(char)
-
+            # Get the position in AVAILABLE_CHARS of that char
+            pos = self.AVAILABLE_CHARS.index(char)
             # Calculate the shift based on the current character in the keyword
-            key_char = self.key[key_pos]
-            shift = self.alphabet.index(key_char)
-
+            shift = self.AVAILABLE_CHARS.index(self.key[key_pos])
             # If the copher is being reversed, make the shift negative.
             if reverse:
                 shift *= -1
 
             # Add shifted character to the string
-            new_pos = (pos + shift) % len(self.alphabet)
-            shifted += self.alphabet[new_pos]
+            new_pos = (pos + shift) % len(self.AVAILABLE_CHARS)
+            shifted += self.AVAILABLE_CHARS[new_pos]
 
             # Shift key position up 1, mod its length.
             key_pos = (key_pos + 1) % len(self.key)
 
         return shifted
 
+    def encrypt(self, plaintext):
+        """Encrypt plaintext using Vigenere cipher"""
+        self._autokey(plaintext)
+        return self._rotate_string(plaintext)
+
+    def decrypt(self, ciphertext):
+        """Decrypt ciphertext using Vigenere cipher"""
+        return self._rotate_string(ciphertext, True)
+
 
 if __name__ == "__main__":
     # key = "7616864623"
-    key = "9b33a532fd9e6"
+    # plaintext = "helloworld123"
+    # key = "9b33a532fd9e6"
+    # plaintext = "thismagazineisavailableinanybigcityinjapan123456789"
+    key = "DeT3Qhx6j8SQ7OL6PwlsHjcha9JUpyXD"
+    plaintext = "456ThismagazineisavailableinanybigcityinJapanShemiscalculatedtheamountofbrothinhersoupandinadvertentlyboileditalloff123"
     print("original key: " + key)
-    # key_list = [int(char) for char in key]
-    g = Gronsfeld(key)
-    # encrypted = g.encrypt("helloworld123")
-    encrypted = g.encrypt("thismagazineisavailableinanybigcityinjapan")
-    print(encrypted)
-    decrypted = g.decrypt(encrypted)
-    print(decrypted)
+    gronsfeld = Gronsfeld(key)
+    encrypted = gronsfeld.encrypt(plaintext)
+    print("encrypted: " + encrypted)
+    decrypted = gronsfeld.decrypt(encrypted)
+    print("decrypted: " + decrypted)
