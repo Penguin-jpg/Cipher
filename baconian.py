d92566a8ee464b6f52cd5035af4f4db31d9bb226
Penguin.jpg <sky026912@gmail.com>
2023-04-11 11:25:51 +0800

    調整baconian.py

--- baconian.py
+++ baconian.py
@@ -1,88 +1,78 @@
-# Python program to implement Baconian cipher
-
-'''This script uses a dictionary instead of 'chr()' & 'ord()' function'''
-
-'''
-Dictionary to map plaintext with ciphertext
-(key:value) => (plaintext:ciphertext)
-This script uses the 26 letter baconian cipher
-in which I, J & U, V have distinct patterns
-'''
-lookup = {'A': 'aaaaaa', 'B': 'aaaaba', 'C': 'aaabaa', 'D': 'aaabba', 'E': 'aabaaa',
-          'F': 'aababa', 'G': 'aabbaa', 'H': 'aabbba', 'I': 'abaaaa', 'J': 'abaaba',
-          'K': 'ababaa', 'L': 'ababba', 'M': 'abbaaa', 'N': 'abbaba', 'O': 'abbbaa',
-          'P': 'abbbba', 'Q': 'baaaaa', 'R': 'baaaba', 'S': 'baabaa', 'T': 'baabba',
-          'U': 'babaaa', 'V': 'bababa', 'W': 'babbaa', 'X': 'babbba', 'Y': 'bbaaaa',
-          'Z': 'bbaaba', '0': 'aaaaab', '1': 'aaaabb', '2': 'aaabab', '3': 'aaabbb',
-          '4': 'aabaab', '5': 'aababb', '6': 'aabbab', '7': 'aabbbb', '8': 'abaaab', '9': 'abaabb'}
-# Added Numbers and an extra digit, because 2**6 > 36 > 2**5.
-# Letters end in a, numbers end in b.
-
-# Function to encrypt the string according to the cipher provided
+"""
+Code is based on https://www.geeksforgeeks.org/baconian-cipher/
+"""
 
 
-def encrypt(message):
-    cipher = ''
-    for letter in message:
-        # checks for space
-        if (letter != ' '):
-            # adds the ciphertext corresponding to the
-            # plaintext from the dictionary
-            cipher += lookup[letter]
-        else:
-            # adds space
-            cipher += ' '
+class Baconian:
+    def __init__(self):
+        # Added Numbers and an extra digit, because 2**6 > 36 > 2**5.
+        # Letters end in a, numbers end in b.
+        self.table = {
+            "A": "aaaaaa",
+            "B": "aaaaba",
+            "C": "aaabaa",
+            "D": "aaabba",
+            "E": "aabaaa",
+            "F": "aababa",
+            "G": "aabbaa",
+            "H": "aabbba",
+            "I": "abaaaa",
+            "J": "abaaba",
+            "K": "ababaa",
+            "L": "ababba",
+            "M": "abbaaa",
+            "N": "abbaba",
+            "O": "abbbaa",
+            "P": "abbbba",
+            "Q": "baaaaa",
+            "R": "baaaba",
+            "S": "baabaa",
+            "T": "baabba",
+            "U": "babaaa",
+            "V": "bababa",
+            "W": "babbaa",
+            "X": "babbba",
+            "Y": "bbaaaa",
+            "Z": "bbaaba",
+            "0": "aaaaab",
+            "1": "aaaabb",
+            "2": "aaabab",
+            "3": "aaabbb",
+            "4": "aabaab",
+            "5": "aababb",
+            "6": "aabbab",
+            "7": "aabbbb",
+            "8": "abaaab",
+            "9": "abaabb",
+        }
+        self.reverse_table = {value: key for key, value in self.table.items()}
 
-    return cipher
+    def encrypt(self, plaintext):
+        """Encrypt plaintext using Baconian cipher"""
+        encrypted = ""
+        for char in plaintext:
+            encrypted += self.table[char] if char != " " else " "
+        return encrypted
 
-
-# Function to decrypt the string
-# according to the cipher provided
-
-
-def decrypt(message):
-    decipher = ''
-    i = 0
-
-    # emulating a do-while loop
-    while True:
-        # condition to run decryption till
-        # the last set of ciphertext
-        if (i < len(message) - 5):
-            # extracting a set of ciphertext
-            # from the message
-            substr = message[i:i + 6]
-            # checking for space as the first
-            # character of the substring
-            if (substr[0] != ' '):
-                '''
-                This statement gets us the key(plaintext) using the values(ciphertext)
-                Just the reverse of what we were doing in encrypt function
-                '''
-                decipher += list(lookup.keys()
-                                 )[list(lookup.values()).index(substr)]
-                i += 6  # to get the next set of ciphertext
-
+    def decrypt(self, ciphertext):
+        """Decrypt ciphertext using Baconian cipher"""
+        decrypted = ""
+        while len(ciphertext) > 0:
+            sub = ciphertext[:6]
+            if sub[0] != " ":
+                decrypted += self.reverse_table[sub]
+                ciphertext = ciphertext[6:]
             else:
-                # adds space
-                decipher += ' '
-                i += 1  # index next to the space
-        else:
-            break  # emulating a do-while loop
-
-    return decipher
+                decrypted += " "
+                ciphertext = ciphertext[1:]
+        return decrypted
 
 
-def main():
-    message = "helloworld123"
-    result = encrypt(message.upper())
-    print(result)
-
-    # message = "AABAAABBABABAABABBBABBAAA"
-    result = decrypt(result.lower())
-    print(result)
-
-
-# Executes the main function
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    # key = ""
+    plaintext = "helloworld123"
+    baconian = Baconian()
+    encrypted = baconian.encrypt(plaintext.upper())
+    print("encrypted: " + encrypted)
+    decrypted = baconian.decrypt(encrypted)
+    print("decryted: " + decrypted.lower())
