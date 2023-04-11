057836ee508e164e3bcc225140d7a430249288a3
Penguin.jpg <sky026912@gmail.com>
2023-04-11 17:35:20 +0800

    修改row_transposition.py程式邏輯

--- row_transposition.py
+++ row_transposition.py
@@ -1,85 +1,69 @@
 import re
+import math
+
 
 class RowTransposition:
     def __init__(self, key, text):
         self.key = self._key_transform(key)
         self.num_cols = len(self.key)
-        self.num_rows = -(-len(text) // self.num_cols)
+        self.num_rows = math.ceil(len(text) / self.num_cols)
 
     def _key_transform(self, key):
-        key_transferred = ""
-        key_digital = ""
-
-        # 先把key中的數字提取出來
-        for d in re.findall(r'\d+',key):
-            key_digital += str(d)
-        
-        # 判斷key中重複的數字
-        for i in key_digital:
-            if i not in key_transferred: 
-                key_transferred += i
+        """Make key valid for Grille cipher"""
+        # 先把key中的數字提取出來並轉成set確保不重複
+        digits = set(re.findall(r"\d+", key))
+        # 將數字合成字串
+        numeric_key = "".join(digits)
 
         index = 0
         # 把缺的數字補進去變連續數字
-        for i in range(1,int(max(key_transferred))):
-            if str(i) not in key_transferred:
-                key_transferred = key_transferred[:index] + str(i) + key_transferred[index:]
+        for i in range(1, int(max(numeric_key))):
+            if str(i) not in numeric_key:
+                numeric_key = numeric_key[:index] + str(i) + numeric_key[index:]
                 index += 2
 
-        return key_transferred
+        return numeric_key
 
     def encrypt(self, plaintext):
+        """Encrpyt plaintext using Row transposition cipher"""
         ciphertext = ""
-        table = [[''] * self.num_cols for _ in range(self.num_rows)]
+        table = [[""] * self.num_cols for _ in range(self.num_rows)]
 
         num_x = self.num_cols - (len(plaintext) % self.num_cols)
+        # 補x到指定長度
         if num_x != 0:
-            plaintext += 'x' * num_x
+            plaintext += "x" * num_x
 
-        row_index = 0
-        col_index = 0
-        
+        row_index, col_index = 0, 0
         for char in plaintext:
             table[row_index][col_index] = char
             col_index += 1
             if col_index == self.num_cols:
                 col_index = 0
                 row_index += 1
-        
+
         for i in range(self.num_cols):
-            col = self.key.index(str(i+1))
+            col = self.key.index(str(i + 1))
             for j in range(self.num_rows):
                 ciphertext += table[j][col]
-        
+
         return ciphertext
 
-
     def decrypt(self, ciphertext):
+        """Decrypt ciphertext using Row transposition cipher"""
         plaintext = ""
-        table = [[''] * self.num_cols for _ in range(self.num_rows)]
+        table = [[""] * self.num_cols for _ in range(self.num_rows)]
 
-        row_index = 0
-        col_index = 0
-        
-        for char in ciphertext:
-            table[row_index][col_index] = "*"
-            col_index += 1
-            if col_index == self.num_cols:
-                col_index = 0
-                row_index += 1
-        
         index = 0
         for i in range(self.num_cols):
-            col = self.key.index(str(i+1))
+            col = self.key.index(str(i + 1))
             for j in range(self.num_rows):
                 table[j][col] = ciphertext[index]
                 index += 1
-        
+
         for row in table:
             plaintext += "".join(row)
-        
-        plaintext = plaintext.replace("*", "")
-        
+
         return plaintext
 
 
